e4f0c924be25e1f73b4d9fd1251e1b6bdf10f489
B00165442 Tobby Akinwale <B00165442@mytudublin.ie>
2025-10-08 10:45:26 +0100

    added newMember.html

---
+++ newMember.php
@@ -0,0 +1,12 @@
+<HTML>
+	<BODY>
+		<FORM method="POST" action="part2.php">
+			<input type="text" name="firstname"><br>
+			<input type="text" name="surname"><br>
+			<input type="submit"><br>
+		</FORM>
+	
+	</BODY>
+
+
+</HTML>
